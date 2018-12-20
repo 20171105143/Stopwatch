8ff773767191b3a0e3fc8b32d7c6250e7744395c
20171105143 <2468900412@qq.com>
2018-12-20 11:29:52 +0800

    1.0
    定义了秒表所需变量,和秒表按钮

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TimeLable: UILabel!
+
+    @IBOutlet weak var PauseButton: UIButton!
+    
+    @IBOutlet weak var PlayButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    var Count = 0.00
+    var Playing = false  //计时状态＝否
 
 }
 
