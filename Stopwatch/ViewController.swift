fdf48fcc7b54d39f087d3af86b036e9f2eae0dae
20171105143 <2468900412@qq.com>
2019-01-06 12:37:10 +0800

    1.3

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var TimeLable: UILabel!
-
-    @IBOutlet weak var PauseButton: UIButton!
     
-    @IBOutlet weak var PlayButton: UIButton!
+    @IBOutlet weak var secondD: UITextField!
+    
+    @IBOutlet weak var minutedD: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,9 +21,36 @@ class ViewController: UIViewController {
 
     var Count = 0.00
     var Playing = false  //计时状态＝否
-
-    override func viewDidLoad() { 
-        super.viewDidLoad()
+    var second = 60
+    var minute = 2
+    var temp = ""
+    var temp2 = ""
+    var timer :Timer!
+    
+    @IBAction func start(_ sender: Any) {
+        temp = secondD.text!
+        second = Int(temp)!
+        temp2 = minutedD.text!
+        minute = Int(temp2)!
+        minutedD.isEnabled = false
+        secondD.isEnabled = false
+        timer = Timer.scheduledTimer(timeInterval: TimeInterval(1),target:self,selector: #selector(tickDown),userInfo:nil,repeats:true)
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        timer.invalidate()
+    }
+    @objc func tickDown(){
+        second += 1
+        if second == 60{
+            second = 0
+            minute+=1
+            if minute == 60{
+                timer.invalidate()
+                
+            }
+        }
+        secondD.text = "\(second)"
+        minutedD.text = "\(minute)"
     }
 }
-
